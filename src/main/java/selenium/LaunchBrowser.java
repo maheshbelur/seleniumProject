de3708ec014b24e219dbd1cdece7fe88e20544ce
Rashmi Mahesh <rashmimahesh@Rashmis-MacBook-Pro.local>
2022-01-09 01:36:31 -0500

    updated

--- src/main/java/selenium/LaunchBrowser.java
+++ src/main/java/selenium/LaunchBrowser.java
@@ -1,6 +1,10 @@
 package selenium;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchBrowser {
@@ -9,10 +13,11 @@ public class LaunchBrowser {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/rashmimahesh/Documents/Seleniumfiles/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
+		driver.get("http://www.amazon.com");
 		driver.manage().window().maximize();
 		String title = driver.getTitle();
 		System.out.println("Title"+ title);
+		
 		
 	}
 	
