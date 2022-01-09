48bb41ad4a2692d25ce92f4b491d2441b39be2e8
Rashmi Mahesh <rashmimahesh@Rashmis-MacBook-Pro.local>
2022-01-09 14:06:51 -0500

    room booking class added

--- src/main/java/selenium/LaunchBrowser.java
+++ src/main/java/selenium/LaunchBrowser.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class LaunchBrowser {
 
@@ -13,10 +14,23 @@ public class LaunchBrowser {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/rashmimahesh/Documents/Seleniumfiles/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.amazon.com");
+		driver.get("https://www.caesars.com/");
 		driver.manage().window().maximize();
 		String title = driver.getTitle();
 		System.out.println("Title"+ title);
+		
+		WebElement bookAroom = driver.findElement(By.xpath("//a[contains(@title,'Book A Room')]"));
+		bookAroom.click();
+		
+		String text = driver.findElement(By.xpath("//*[contains(text(),'Best Rate Guarantee')]")).getText();
+		System.out.println("test="+text);
+		
+		//driver.findElement(By.id("dropdown_2__popout")).click();
+		//Select select = new Select(driver.findElement(By.id("dropdown_2__popout")));
+		//select.selectByVisibleText("Flamingo Las Vegas");
+		
+		
+		
 		
 		
 	}
---
+++ src/main/java/selenium/TC_RoomBooking.java
@@ -0,0 +1,5 @@
+package selenium;
+
+public class TC_RoomBooking {
+
+}
